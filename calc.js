832a51c5c6052c9d07bec4f655c74079e454f690
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-01-18 15:08:35 -0600

    All operator implementations working

--- calc.js
+++ calc.js
@@ -14,6 +14,32 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+") {
+		
+		console.log(num1 + num2);
+
+	} else if (mathSymbol === "-") {
+		
+		console.log(num1 - num2);
+
+	} else if (mathSymbol === "*") {
+		
+		console.log(num1 * num2);
+
+	} else if (mathSymbol === "/") {
+		
+		console.log(num1 / num2);
+
+	} else if (mathSymbol === "sqrt") {
+		
+		console.log(Math.sqrt(num1))
+		console.log(Math.sqrt(num2))
+		
+	} else {
+
+		console.log("Error");
+	}
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
